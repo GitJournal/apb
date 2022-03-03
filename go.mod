576b07cacaad5ed78ee17488b002d62e8fd465ab
Vishesh Handa <me@vhanda.in>
2022-03-03 13:11:20 +0100

    Add a proper CLI interface

--- go.mod
+++ go.mod
@@ -4,14 +4,18 @@ go 1.17
 
 require (
 	github.com/davecgh/go-spew v1.1.0
+	github.com/urfave/cli/v2 v2.3.0
 	google.golang.org/api v0.70.0
 )
 
 require (
 	cloud.google.com/go/compute v1.3.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
--- main.go
+++ main.go
@@ -9,46 +9,83 @@ import (
 
 	"google.golang.org/api/androidpublisher/v3"
 	"google.golang.org/api/option"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) != 2 {
-		fmt.Println("Usage: packageId credentialsFilePath")
-		os.Exit(1)
-	}
-	pkg := args[0]
-	credsFile := args[1]
+	var credsFile string
+	var pkg string
 
-	ctx := context.Background()
-	androidpublisherService, err := androidpublisher.NewService(ctx, option.WithCredentialsFile(credsFile))
+	app := &cli.App{
+		Name:  "Android Publisher",
+		Usage: "Access the Android Publisher API",
+
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "cred",
+				Aliases:     []string{"c"},
+				Value:       "google-play-api-key.json",
+				Usage:       "Load Google Play API Credentials from `FILE`",
+				Destination: &credsFile,
+				EnvVars:     []string{"GOOGLE_PLAY_API_CREDENTIALS_FILE"},
+			},
+			&cli.StringFlag{
+				Name:        "package",
+				Aliases:     []string{"p"},
+				Usage:       "App Package ID",
+				Destination: &pkg,
+				EnvVars:     []string{"APB_PACKAGE"},
+				Required:    true,
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "trackInfo",
+				Usage: "Display Info about a track",
+				Action: func(c *cli.Context) error {
+					return trackInfo(c.Context, pkg, credsFile)
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func trackInfo(ctx context.Context, pkg string, credsFile string) error {
+	androidpublisherService, err := androidpublisher.NewService(ctx, option.WithCredentialsFile(credsFile))
+	if err != nil {
+		return err
 	}
 
 	var appEdit androidpublisher.AppEdit
-	c1 := androidpublisherService.Edits.Insert(pkg, &appEdit)
-	rsp, err := c1.Do()
+	editCreate := androidpublisherService.Edits.Insert(pkg, &appEdit)
+	editCreateRsp, err := editCreate.Do()
 	if err != nil {
-		log.Fatal(err)
+		return err
+	}
+	editId := editCreateRsp.Id
+
+	c := androidpublisherService.Edits.Tracks.Get(pkg, editId, "production")
+	rsp, err := c.Do()
+	if err != nil {
+		return err
 	}
 
-	editId := rsp.Id
-
-	c3 := androidpublisherService.Edits.Tracks.Get(pkg, editId, "production")
-	rsp3, err := c3.Do()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if len(rsp3.Releases) != 1 {
+	if len(rsp.Releases) != 1 {
 		log.Fatal("Should have received one response")
 	}
-	release := rsp3.Releases[0]
+	release := rsp.Releases[0]
 
 	json, err := json.MarshalIndent(release, "", "  ")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fmt.Println(string(json))
+
+	return nil
 }
